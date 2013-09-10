3470118fee1b3c0feefe31d1e5d310ffcf62c166
MakerSquare Student <students@themakersquare.com>
2013-09-10 16:23:39 -0500

    Adds get_hit method to Person class and punch and use_megazord to Powerranger class

--- powerranger.rb
+++ powerranger.rb
@@ -1,8 +1,18 @@
 class Person
+attr_accessor :name, :caffeine
 
 	def initialize(name, caffeine)
 	@name = name
 	@caffeine = caffeine
+	end
+
+	def get_hit(strength)
+		if strength > 5
+			puts "#{@name} runs away, terrified! " * 2
+		else
+			puts "#{@name} runs away, terrified!"
+		end
+		@caffeine -= 1
 	end
 
 	def run(miles)
@@ -19,8 +29,43 @@ class Person
 
 end
 
-class PowerRanger
+class PowerRanger < Person
+	attr_accessor :strength
+	
+	def initialize(name, caffeine)
+		super
+		@strength = 0
+		@color = nil
+	end
+
+	def punch(person)
+		person.get_hit(@strength)
+		@caffeine -= 1
+	end
+
+	def use_megazord(person)
+		person.get_hit(5000)
+		@caffeine -= 1
+	end
+
+	def rest
+	end
+
 end
 
 class EvilNinja
 end
+
+
+me = Person.new("Joe", 10)
+super_me = PowerRanger.new("Super Joe", 15)
+
+
+puts "getting punched"
+super_me.punch(me)
+puts "getting megazorded"
+super_me.use_megazord(me)
+
+
+
+
