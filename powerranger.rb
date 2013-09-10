c3fbf3a3a272c683d7b1dfff73c3e79d97f5e3fd
MakerSquare Student <students@themakersquare.com>
2013-09-10 15:41:00 -0500

    Adds the run, scream, and drink_coffee methods to the Person class, initializes name and caffeine instance variables.

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,22 @@
 class Person
+
+	def initialize(name, caffeine)
+	@name = name
+	@caffeine = caffeine
+	end
+
+	def run(miles)
+		puts "You ran #{miles} miles!"
+	end
+
+	def scream(words)
+		puts "#{words.upcase}!"  
+	end
+
+	def drink_coffee
+		@caffeine += 1
+	end
+
 end
 
 class PowerRanger
